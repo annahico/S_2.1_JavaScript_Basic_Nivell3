10cca19f29b6193037daeaaf56162bea528fa425
annahico <annahico@gmail.com>
2025-07-10 11:57:38 +0200

    feat: added first function and used with node

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// node js/script.js
+function add(a, b) {
+    return a + b;
+}
+
+// llamar a la función y mostrar el resultado
+console.log(add(5, 3)); // 8
+console.log("Suma de -1 y 10:", add(-1, 10)); // 9
