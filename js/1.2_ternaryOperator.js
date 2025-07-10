1a95ae0f9082f357d9c2ea3cce34a972bf46b115
annahico <annahico@gmail.com>
2025-07-10 13:04:24 +0200

    feat: created ternary branch

---
+++ js/1.2_ternaryOperator.js
@@ -0,0 +1,7 @@
+// node 1.2_ternaryOperator/script.js
+
+// ***EXERCICI 1***:
+// Operador ternari bàsic: 
+// Escriu una funció potConduir que accepti l'edat com a paràmetre i utilitzi l'operador ternari per determinar si l'usuari pot conduir. Si l'edat és 18 o més, ha de retornar 'Pots conduir'. 
+// Si no, ha de retornar 'No pots conduir'.
+
