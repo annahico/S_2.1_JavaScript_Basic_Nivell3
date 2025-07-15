fbd4542060d0d7d7ef9576ad110859ab256a0a6b
annahico <annahico@gmail.com>
2025-07-15 13:23:29 +0200

    feat: added ex 5

--- js/1.5_arrayTransformations.js
+++ js/1.5_arrayTransformations.js
@@ -33,3 +33,23 @@ console.log(majorDe10);
 const numeros4 = [13, 7, 8, 21];
 const sumaTotal = numeros4.reduce((acc, num) => acc + num, 0);
 console.log(sumaTotal);
+
+
+//** Exercici 5 */
+// Donat un array "[ 1, 3, 7, 10 ,15, 17, 11, 5, 8, 12, 9 ]", crea una funció en una sola línia que faci el següent:
+
+// - Filtra els nombres majors o iguals a 10.
+
+// - Multiplica cada nombre filtrat per 2.
+
+// - Calcula la suma dels nombres filtrats i multiplicats per 2.
+
+// - La funció ha de retornar el resultat de la suma.
+
+const numeros5 = [1, 3, 7, 10, 15, 17, 11, 5, 8, 12, 9];
+const resultat = numeros5
+    .filter(num => num >= 10)
+    .map(num => num * 2)
+    .reduce((acc, num) => acc + num, 0);
+    console.log(resultat);
+    
